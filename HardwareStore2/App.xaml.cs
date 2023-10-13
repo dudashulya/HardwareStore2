db6026b2a4dfb2ff189ee94b7a555eb8390578ad
212112 <marushkinanastena@gmail.com>
2023-10-13 14:47:38 +0300

    add database

--- HardwareStore2/App.xaml.cs
+++ HardwareStore2/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HardwareStore2.Base;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,6 @@ namespace HardwareStore2
     /// </summary>
     public partial class App : Application
     {
+        public static HardwareStoreEntities db = new HardwareStoreEntities ();
     }
 }
