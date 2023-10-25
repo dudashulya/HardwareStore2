407fbb9db8c0ce56d7dbe67776d670ff2929eda0
212112 <marushkinanastena@gmail.com>
2023-10-25 10:57:32 +0300

    add sort and serch

--- HardwareStore2/Components/ServicePage.xaml.cs
+++ HardwareStore2/Components/ServicePage.xaml.cs
@@ -24,6 +24,7 @@ namespace HardwareStore2.Components
         public ServicePage()
         {
             InitializeComponent();
+            Refresh();
             IEnumerable<Product> List = App.db.Product;
             foreach(Product Item in List)
             {
@@ -31,7 +32,72 @@ namespace HardwareStore2.Components
             } 
             
         }
-        
+        private void Refresh()
+        {
+            IEnumerable<Product> serviceSortList = App.db.Product;
+            if (ShortCB.SelectedIndex != 0)
+            {
+                if (ShortCB.SelectedIndex == 1)
+                {
+                    serviceSortList = serviceSortList.OrderBy(x => x.Cost);
+                }
+                else if (ShortCB.SelectedIndex == 2)
+                {
+                    serviceSortList = serviceSortList.OrderByDescending(x => x.Cost);
+                }
+            }
+            if (DiscountFilterCb.SelectedIndex != 0)//выводим сортировку комбобокс
+            {
+                if (DiscountFilterCb.SelectedIndex == 1)
+                {
+                    serviceSortList = serviceSortList.Where(x => x.Discount == 0 && x.Discount < 2);
+                }
+                if (DiscountFilterCb.SelectedIndex == 2)
+                {
+                    serviceSortList = serviceSortList.Where(x => x.Discount >= 2 && x.Discount < 5);
+                }
+                if (DiscountFilterCb.SelectedIndex == 3)
+                {
+                    serviceSortList = serviceSortList.Where(x => x.Discount >= 5 && x.Discount < 30);
+                }
+
+                if (SerchTb.Text != null)
+                {
+                    serviceSortList = serviceSortList.Where(x => x.Title.ToLower().Contains(SerchTb.Text.ToLower()) || x.Description.ToLower().Contains(SerchTb.Text.ToLower())); //поиск по слову
+                }
+
+                //всегда в конце так как сортировки могут наслаиваться 
+                ServicesWp.Children.Clear();
+                foreach (var service in serviceSortList)
+                {
+                    ServicesWp.Children.Add(new ServiceUserControl1(service));
+
+
+
+                }
+
+
+
+
+            }
+
+        }
+
+        private void ShortCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void DiscountFilterCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void SerchTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Refresh();
+        }
+
         //
     }
 }
